b8176ee8e64255b3dd28cad24464b1d815ec0676
Miquel Angel Quinones Garcia <m.quinones.garcia@gmail.com>
2013-02-22 23:22:13 +0100

    add some comments :)

--- test/test/NSBundle+LocalizationChecker.m
+++ test/test/NSBundle+LocalizationChecker.m
@@ -15,6 +15,7 @@ static Method originalMethod;
 
 @implementation NSBundle (LocalizationChecker)
 + (void) initialize {
+    //Swizzle method implementations which get string from NSBundle strings file
     if (self == [NSBundle class]) {
         originalMethod = class_getInstanceMethod(self, @selector(localizedStringForKey:value:table:));
          Method mine = class_getInstanceMethod(self, @selector(swappedLocalizedStringForKey:value:table:));
@@ -23,18 +24,19 @@ static Method originalMethod;
 }
 
 - (NSString *)swappedLocalizedStringForKey:(NSString *)key value:(NSString *)value table:(NSString *)tableName {
-
-   //put in dict key
-    
+    //Get the original (from file or not) translated string
     NSString* translated = [self swappedLocalizedStringForKey:key value:value table:tableName];
     
+    //Apple docs state:
+    //A localized version of the string designated by key in table tableName.
+    //If value is nil or an empty string, and a localized string is not found in the table, returns key.
+    //If key and value are both nil, returns the empty string.
     
+    //Need to check if programmer supplied default key
     BOOL defaultSupplied = (value != nil) && value.length > 0;
-    
     if (!defaultSupplied) {
         value = @"__MYHACK_DEFAULTVAULE__";
     }
-    
     
     BOOL keyIsSameAsReturned = (key == translated);
     
@@ -42,20 +44,15 @@ static Method originalMethod;
         ||
         (defaultSupplied && [translated isEqualToString:value])) {
 
-        //we don't have the string localized
+        //We don't have the string localized
         NSLog(@"*****NO STRING IS LOCALIZED********");
-        
     } else {
-        //string localized, add to structure
-        
-        
+        //String localized, add to structure
+        //This will be checked later on when a string is set to a UI component
         [[LocalizationChecker sharedLocalizationChecker] addLocalizedWord:translated];
-        
     }
     
-    
     return translated;
-    
 }
 
 @end
