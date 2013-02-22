24d045a862a0b8237e86e3244f3a6d3e9df72548
Hector Zarate <hecktorzr@gmail.com>
2013-02-22 21:30:49 +0100

    Verifying UILabel's text setup from IB

--- test/test/UILabel+LocalizationChecker.m
+++ test/test/UILabel+LocalizationChecker.m
@@ -10,6 +10,14 @@
 #import "LocalizationChecker.h"
 #import <objc/runtime.h>
 
+//@interface UILabel ()
+//@property (nonatomic, strong) UIColor *originalBackgroundColor;
+//@end
+//
+//@implementation UILabel
+//@synthesize originalBackgroundColor;
+//@end
+
 @implementation UILabel (LocalizationChecker)
 
 // What happens when text is called from IB ?
@@ -21,21 +29,40 @@
 }
 
 + (void)setup {
+    // swizzle setText
     Method originalMethod = class_getInstanceMethod(self, @selector(setText:));
     
     Method mine = class_getInstanceMethod(self, @selector(swappedSetText:));
     method_exchangeImplementations(originalMethod, mine);
+    
+    // swizzle setValue:forKey:
+    Method originalSetValueMethod = class_getInstanceMethod(self, @selector(awakeFromNib));
+    Method mineSetValueMethod = class_getInstanceMethod(self, @selector(swappedAwakeFromNib));
+    method_exchangeImplementations(originalSetValueMethod, mineSetValueMethod);
 }
 
 - (void)swappedSetText:(NSString *)text {
     
     if ([[LocalizationChecker sharedLocalizationChecker] isStringLocalized:text] == NO) {
         [self setBackgroundColor:[UIColor redColor]];
+    } else {
+//        [self setBackgroundColor:self.originalBackgroundColor];
     }
     
     [self swappedSetText:text];
 }
 
-
+- (void)swappedAwakeFromNib
+{
+    if ([self isKindOfClass:[UILabel class]])
+    {
+        [self swappedAwakeFromNib];
+        
+        if ([[LocalizationChecker sharedLocalizationChecker] isStringLocalized:self.text] == NO) {
+//            self.originalBackgroundColor = self.backgroundColor;
+            [self setBackgroundColor:[UIColor redColor]];
+        }
+    }
+}
 
 @end
