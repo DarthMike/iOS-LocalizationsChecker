118a7ca1883c24deab65efd7c6a66db8772399d9
Hector Zarate <hecktorzr@gmail.com>
2013-02-22 20:38:46 +0100

    Method swizzling for UILabel

---
+++ test/test/UILabel+LocalizationChecker.h
@@ -0,0 +1,13 @@
+//
+//  UILabel+LocalizationChecker.h
+//  test
+//
+//  Created by Hector Zarate on 2/22/13.
+//  Copyright (c) 2013 Miquel Angel Quinones Garcia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (LocalizationChecker)
+
+@end
