3807f05774b217f36569d22f2181f9b1fad2bda1
Hector Zarate <hecktorzr@gmail.com>
2013-02-22 20:38:59 +0100

    Sample UILabel

--- test/test/ViewController.h
+++ test/test/ViewController.h
@@ -10,4 +10,7 @@
 
 @interface ViewController : UIViewController
 
+
+@property (weak, nonatomic) IBOutlet UILabel *coolLabel;
+
 @end
--- test/test/ViewController.m
+++ test/test/ViewController.m
@@ -17,7 +17,9 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    self.coolLabel.text = @"hello";
+//    [self.coolLabel setText:@"hi"];
 }
 
 @end
